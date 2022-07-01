2e4f793a7c6d120459f74a44750128c48881ae51
marcos.gonzalez <marcos.gonzalez.marcial@hotmail.com>
2022-07-01 13:35:22 +0200

    hero-bg updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,39 @@ export const Hero = () => {
   hero.setAttribute("id", "home");
 
   hero.innerHTML = `
-    <div class="container">
-      <div class="hero-content">
-        <p class="hero-subtitle">¡Déjate sorprender!</p>
-
-        <h1 class="h1 hero-title">Amara style Fashion house</h1>
-
-        <button class="btn btn-primary">Compra aquí</button>
-      </div>
-    </div>
+  <div class="hero-content">
+  
+  
+  <h1 class="h1 hero-title-left">Amara style </h1>
+  <button class="btn btn-secondary-bg">DESCÚBRENOS</button>
+  <h1 class="h1 hero-title-right">Fashion house</h1>
+  
+  <div class="hero-left">
+  <button class="btn btn-secondary-bg-xxl">MODA MUJER</button>
+  </div>
+  <div class="hero-right">
+  <button class="btn btn-secondary-bg-xxl">MODA HOMBRE</button>
+  </div>
+</div>
 `;
   return hero;
 };
+
+// <div class="hero-content">
+//   <p class="hero-subtitle">¡Déjate sorprender!</p>
+
+//   <h1 class="h1 hero-title">Amara style Fashion house</h1>
+
+//   <button class="btn btn-primary">Compra aquí</button>
+// </div>
+
+{
+  /* <div class="container">
+  <div class="hero-content">
+    <div class="hero-left"></div>
+    <div class="hero-right">
+      <button class="btn btn-secondary">Compra aquí</button>
+    </div>
+  </div>
+</div>; */
+}
