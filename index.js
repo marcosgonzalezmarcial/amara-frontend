dd422d56f76656e8b54cabf4ff0d2aec363e8ad4
marcosgonzalezmarcial <marcospgonzalezm@gmail.com>
2022-08-31 14:46:24 +0200

    loader function isolated on its own file

--- index.js
+++ index.js
@@ -1,94 +1,98 @@
-import { App } from "./src/App";
-import { deleteItem } from "./src/ui/ui-utils/deleteItem";
-import { paintProducts } from "./src/ui/ui-utils/paintProducts";
+import { App } from './src/App'
+import { deleteItem } from './src/ui/ui-utils/deleteItem'
+import launchLoader from './src/ui/ui-utils/launchLoader'
+import { paintProducts } from './src/ui/ui-utils/paintProducts'
 
-if (!document.getElementById("root").innerHTML) {
-  document.querySelector("body").appendChild(App());
+if (!document.getElementById('root').innerHTML) {
+  document.querySelector('body').appendChild(App())
 }
 
 /* DOM Elements variables */
 const actionsButtons = [
-  ...document.querySelectorAll("button.header-action-btn")
-];
+  ...document.querySelectorAll('button.header-action-btn')
+]
 
-const categoryButtons = [...document.querySelectorAll(".filter-btn")];
-const shoppingBagEl = document.querySelector(".shopping-bag-container");
-const shopBagBtn = document.querySelector(".shopping-bag-btn");
-const shopBagCloseBtn = document.querySelector(".bag-close-btn");
+const categoryButtons = [...document.querySelectorAll('.filter-btn')]
+const shoppingBagEl = document.querySelector('.shopping-bag-container')
+const shopBagBtn = document.querySelector('.shopping-bag-btn')
+const shopBagCloseBtn = document.querySelector('.bag-close-btn')
 
 /*****************************************
  ******** shopping-bag display logic ************/
 
 actionsButtons.forEach((btn) => {
-  btn.addEventListener("click", function () {
+  btn.addEventListener('click', function () {
     // removing active class from all the buttons except the clicked one
     actionsButtons.forEach(
-      (btn) => this !== btn && btn.classList.remove("active")
-    );
-    this.classList.toggle("active");
-  });
+      (btn) => this !== btn && btn.classList.remove('active')
+    )
+    this.classList.toggle('active')
+  })
 
   // show and hide bag when the action buttons are clikced
-  btn.children[1].innerText === "Carrito"
-    ? btn.addEventListener("click", toggleBag)
-    : btn.addEventListener("click", hideBag);
-});
+  btn.children[1].innerText === 'Carrito'
+    ? btn.addEventListener('click', toggleBag)
+    : btn.addEventListener('click', hideBag)
+})
 
-export function toggleBag() {
-  document.querySelector(".arrow").classList.toggle("arrow-up");
-  shoppingBagEl.classList.toggle("show-bag");
+export function toggleBag () {
+  document.querySelector('.arrow').classList.toggle('arrow-up')
+  shoppingBagEl.classList.toggle('show-bag')
   // avoid page scrolling on the back when the bag is open
-  document.body.classList.toggle("scroll-control");
+  document.body.classList.toggle('scroll-control')
 }
 
-export function showBag() {
-  if (shoppingBagEl.classList.contains("show-bag")) return;
-  document.querySelector(".arrow").classList.toggle("arrow-up");
-  shoppingBagEl.classList.toggle("show-bag");
+export function showBag () {
+  if (shoppingBagEl.classList.contains('show-bag')) return
+  document.querySelector('.arrow').classList.toggle('arrow-up')
+  shoppingBagEl.classList.toggle('show-bag')
   // avoid page scrolling on the back when the bag is open
-  document.body.classList.toggle("scroll-control");
+  document.body.classList.toggle('scroll-control')
 }
 
-export function hideBag() {
-  document.querySelector(".arrow").classList.remove("arrow-up");
-  shoppingBagEl.classList.remove("show-bag");
+export function hideBag () {
+  document.querySelector('.arrow').classList.remove('arrow-up')
+  shoppingBagEl.classList.remove('show-bag')
 
   // avoid page scrolling on the back when the bag is open
-  document.body.classList.remove("scroll-control");
+  document.body.classList.remove('scroll-control')
 }
 
 /* *************** fetching products by category in Products section ****
  * TODO -> this should be in the produts file ************************* */
 
 // paint products from api on first load category "Más vendidos"
-paintProducts("Más vendidos");
+paintProducts('Más vendidos')
 
 categoryButtons.forEach((btn) => {
-  btn.addEventListener("click", function () {
+  btn.addEventListener('click', function () {
     // removing active class from all the buttons except the clicked one
     categoryButtons.forEach(
-      (btn) => this !== btn && btn.classList.remove("active")
-    );
-    this.classList.toggle("active");
-    paintProducts(btn.firstChild.data);
-  });
-});
+      (btn) => this !== btn && btn.classList.remove('active')
+    )
+    this.classList.toggle('active')
+    paintProducts(btn.firstChild.data)
+  })
+})
 
 /*****************************************
  ******** closing bag logic ************/
-shopBagCloseBtn.addEventListener("click", () => {
-  hideBag();
-  shopBagBtn.classList.toggle("active");
-});
+shopBagCloseBtn.addEventListener('click', () => {
+  hideBag()
+  shopBagBtn.classList.toggle('active')
+})
 
 /*****************************************
  ******** adding delete logic to bag items on first load************/
 const deleteBtns = document.querySelectorAll(
-  ".item-description-bottom-text-right"
-);
+  '.item-description-bottom-text-right'
+)
 deleteBtns.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    event.preventDefault();
-    deleteItem(event.target.dataset.id);
-  });
-});
+  btn.addEventListener('click', (event) => {
+    event.preventDefault()
+    deleteItem(event.target.dataset.id)
+  })
+})
+
+// start loader when page finishes loading resources
+launchLoader()
