5f8ee320ee39bb85b0e52baa9a2ecc505accc130
Marcos González <marcos.gonzalez.marcial@hotmail.com>
2022-09-10 12:36:09 +0200

    BEM naming in hero section finished

--- src/ui/ui-utils/addFavourites.js
+++ src/ui/ui-utils/addFavourites.js
@@ -1,14 +1,15 @@
 export const addFavourites = () => {
   const productList = document.querySelector('.product-list')
-
-  productList.addEventListener('click', (e) => {
-    e.stopPropagation()
-    if (
-      e.target.tagName === 'DIV' &&
-      e.target.classList.contains('heart-icon-wrapper')
-    ) {
-      e.target.children[0].classList.toggle('heart-icon-hide')
-      e.target.children[1].classList.toggle('heart-icon-filled-show')
-    }
-  })
+  if (productList) {
+    productList.addEventListener('click', (e) => {
+      e.stopPropagation()
+      if (
+        e.target.tagName === 'DIV' &&
+        e.target.classList.contains('heart-icon-wrapper')
+      ) {
+        e.target.children[0].classList.toggle('heart-icon-hide')
+        e.target.children[1].classList.toggle('heart-icon-filled-show')
+      }
+    })
+  }
 }
